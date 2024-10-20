dd576f16427dc84f3498a057f5bc0cfd45732b5b
Visal <visalraj.in@gmail.com>
2024-10-20 21:32:58 +0530

    Pnpm regular checks, edit customer actions

--- src/app/admin/customers/[id]/edit/page.tsx
+++ src/app/admin/customers/[id]/edit/page.tsx
@@ -1,5 +1,11 @@
-import Breadcrumbs from "@/app/ui/admin/components/breadcrumbs"
-export default function editCustomer() {
+import Breadcrumbs from "@/app/ui/admin/components/breadcrumbs";
+import EditCustomerForm from "@/app/ui/admin/components/edit-customer-form";
+import { getCustomerById } from "@/app/lib/actions-admins";
+
+export default async function editCustomer({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
+    const customer = await getCustomerById({ id });
+
     return (
         <main>
             <Breadcrumbs
@@ -12,7 +18,10 @@ export default function editCustomer() {
                     },
                 ]}
             />
-
+            {
+                customer ? <EditCustomerForm uuid={id} customer={customer.data} /> : ''
+            }
         </main>
-    )
+    );
 }
+
---
+++ src/app/ui/admin/components/edit-customer-form.tsx
@@ -0,0 +1,61 @@
+import { updateCustomerById } from "@/app/lib/actions-admins";
+import { ActionButtons } from "../dashboard/buttons";
+import { Customer } from "@/app/lib/definitions";
+
+export default function EditCustomerForm({ uuid, customer }: { uuid: string; customer: Customer[] }) {
+    const updateCustomerWithId = updateCustomerById.bind(null, uuid);
+
+    return (
+        <>
+            {customer ? (
+                <>
+                    <form autoComplete="off" action={updateCustomerWithId} className="bg-gray-300 p-5 rounded">
+                        <div className="input-form mb-4">
+                            <label htmlFor="CustomerName" className="flex mb-1">Name</label>
+                            <input
+                                type="text"
+                                name="firstname"
+                                className="w-full p-2 rounded cursor-not-allowed"
+                                defaultValue={`${customer[0].firstname} ${customer[0].lastname}`}
+                                readOnly
+                            />
+                        </div>
+                        <div className="input-form mb-4">
+                            <label htmlFor="CustomerUserName" className="flex mb-1">Username</label>
+                            <input
+                                type="text"
+                                name="username"
+                                className="w-full p-2 rounded"
+                                defaultValue={customer[0].username}
+                            />
+                        </div>
+                        <div className="input-form mb-4">
+                            <label htmlFor="CustomerEmail" className="flex mb-1">Email</label>
+                            <input
+                                type="text"
+                                name="email"
+                                className="w-full p-2 rounded cursor-not-allowed"
+                                defaultValue={customer[0].email}
+                                readOnly
+                            />
+                        </div>
+                        <div className="input-form mb-6">
+                            <label htmlFor="CustomerStatus" className="flex mb-1">Status</label>
+                            <select
+                                className="w-full p-2 rounded"
+                                name="status"
+                                defaultValue={customer[0].isActive ? 'Active' : 'Inactive'}
+                            >
+                                <option value="Active">Active</option>
+                                <option value="Inactive">Inactive</option>
+                            </select>
+                        </div>
+                        <div className="input-form mb-4">
+                            <ActionButtons name="Update" isclicked={false} pointo="" />
+                        </div>
+                    </form>
+                </>
+            ) : ''}
+        </>
+    );
+}
