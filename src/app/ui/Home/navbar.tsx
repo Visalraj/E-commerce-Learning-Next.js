b94cbf4829a05f017d3b27c09e530ec21c789f1c
Visal <visalraj.in@gmail.com>
2024-10-27 11:10:42 +0530

    Code cleaning

--- src/app/ui/Home/navbar.tsx
+++ src/app/ui/Home/navbar.tsx
@@ -36,8 +36,6 @@ export default async function Navbar() {
                                         <Link href={"/login"}><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ml-7 ">Login</button></Link>
                                     </>
                             }
-
-
                         </div>
                     </div>
                     <button data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-search" aria-expanded="false">
