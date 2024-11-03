804e1f2600156b3fcb154a6a0932e4113423c2b5
Visal <visalraj.in@gmail.com>
2024-11-03 14:11:05 +0530

    Form tweaks

--- src/app/ui/Home/register-form.tsx
+++ src/app/ui/Home/register-form.tsx
@@ -1,38 +1,42 @@
 'use server';
+import { CreateButtonComponent } from "../customer/components/create-button";
 export default async function CustomerRegForm() {
     return (
-        <form className="bg-slate-300 p-3 w-fit m-auto rounded top-10 relative">
+        <form className="bg-gray-300 p-3 w-fit m-auto rounded top-10 relative">
             <div className="create-label">
                 <h1 className="font-extrabold text-2xl text-center">Whoo!! Fill out this fields</h1>
             </div>
             <div className="inputFields mt-5 flex">
                 <div className="wrap-field-one">
                     <label htmlFor="Customer Name" className="label-class w-full">Firstname</label>
-                    <input type="text" name="customer_fname" className="w-full p-2 rounded-sm" />
+                    <input type="text" name="customer_fname" className="w-full p-2 rounded-sm border outline-none focus:border-gray-400 bg-gray-100" />
+
                 </div>
                 <div className="wrap-field-two ml-14">
                     <label htmlFor="Customer Name" className="label-class w-full">Lastname</label>
-                    <input type="text" name="customer_lname" className="w-full p-2 rounded-sm" />
+                    <input type="text" name="customer_lname" className="w-full p-2 rounded-sm border outline-none focus:border-gray-400 bg-gray-100" />
                 </div>
 
             </div>
             <div className="inputFields mt-5 flex">
                 <div className="wrap-field-one">
                     <label htmlFor="Customer Name" className="label-class w-20">Age</label>
-                    <input type="text" name="customer_age" className="w-full p-2 rounded-sm" style={{ width: "30px" }} />
+                    <input type="number" name="customer_age" className="w-full p-2 rounded-sm border outline-none focus:border-gray-400 bg-gray-100" />
                 </div>
                 <div className="wrap-field-two ml-14">
                     <label htmlFor="Customer Name" className="label-class" style={{ width: "58px" }}>Email</label>
-                    <input type="email" name="customer_lname" className="w-full p-2 rounded-sm" />
+                    <input type="email" name="customer_lname" className="w-full p-2 rounded-sm border outline-none focus:border-gray-400 bg-gray-100" />
                 </div>
             </div>
             <div className="inputFields mt-5 flex">
                 <div className="wrap-field-two ml-2">
                     <label htmlFor="Customer Name" className="label-class" >Address</label>
-                    <textarea name="" id="" style={{ display: "block" }} cols={66}></textarea>
+                    <textarea className="border outline-none focus:border-gray-400 bg-gray-100" name="" id="" style={{ display: "block" }} cols={66}></textarea>
                 </div>
             </div>
-
+            <div className="inputFields mt-5 flex justify-end">
+                <CreateButtonComponent />
+            </div>
         </form>
     )
 }
---
+++ src/app/ui/customer/components/create-button.tsx
@@ -0,0 +1,7 @@
+'use client';
+import { CreateButton } from "../../admin/components/buttons";
+export const CreateButtonComponent = () => {
+    return (
+        <CreateButton name={"Create"} pointto={''} />
+    )
+}
