e3b6f894407363e0fa25b7d945f84d4154261de3
Visal <visalraj.in@gmail.com>
2024-11-03 11:09:45 +0530

    Registration form, pnpm regular checks

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+'u server';
+import Navbar from "../ui/Home/navbar";
+import CustomerRegForm from "../ui/Home/register-form";
+export default async function Page() {
+    return (
+        <>
+            <Navbar />
+            <CustomerRegForm />
+        </>
+    )
+}
