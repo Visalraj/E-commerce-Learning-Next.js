105f20f1d977a3fc345c19a8f11eff66e1b1cb84
Visal <visalraj.in@gmail.com>
2024-11-09 21:45:45 +0530

    Search tweaks

--- src/app/ui/admin/components/customers-list.tsx
+++ src/app/ui/admin/components/customers-list.tsx
@@ -3,88 +3,95 @@ import { getCustomers } from "@/app/lib/actions-admins";
 import Icon from "../../common/svg-tiles";
 import { ActionButtons } from "./buttons";
 import DeleteCustomer from "./delete-customer";
+import Search from "../../common/search";
 export default async function Customers() {
+
     const response = await getCustomers();
     return (
         <>
             {response && response.status === 200 && (
-                <div className="table-layout-wrap w-full mt-14 m-auto">
-                    <div className="mt-6 flow-root">
-                        <div className="inline-block min-w-full align-middle">
-                            <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
-                                <table className="hidden min-w-full text-gray-900 md:table">
-                                    <thead className="rounded-lg text-left text-sm font-normal">
-                                        <tr>
-                                            <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
-                                                ID
-                                            </th>
-                                            <th scope="col" className="px-3 py-5 font-medium">
-                                                Name
-                                            </th>
-                                            <th scope="col" className="px-3 py-5 font-medium">
-                                                Username
-                                            </th>
-                                            <th scope="col" className="px-3 py-5 font-medium">
-                                                Email
-                                            </th>
-                                            <th scope="col" className="px-3 py-5 font-medium">
-                                                Status
-                                            </th>
-                                            <th scope="col" className="px-3 py-5 font-medium">
-                                                Created Date
-                                            </th>
-                                            <th scope="col" className="px-3 py-5 font-medium">
-                                                Action
-                                            </th>
+                <>
+                    {/* <div className="search-component mt-5">
+                        <Search placeholder="Search here..." />
+                    </div> */}
+                    <div className="table-layout-wrap w-full mt-14 m-auto">
+                        <div className="mt-6 flow-root">
+                            <div className="inline-block min-w-full align-middle">
+                                <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+                                    <table className="hidden min-w-full text-gray-900 md:table">
+                                        <thead className="rounded-lg text-left text-sm font-normal">
+                                            <tr>
+                                                <th scope="col" className="px-4 py-5 font-medium sm:pl-6">
+                                                    ID
+                                                </th>
+                                                <th scope="col" className="px-3 py-5 font-medium">
+                                                    Name
+                                                </th>
+                                                <th scope="col" className="px-3 py-5 font-medium">
+                                                    Username
+                                                </th>
+                                                <th scope="col" className="px-3 py-5 font-medium">
+                                                    Email
+                                                </th>
+                                                <th scope="col" className="px-3 py-5 font-medium">
+                                                    Status
+                                                </th>
+                                                <th scope="col" className="px-3 py-5 font-medium">
+                                                    Created Date
+                                                </th>
+                                                <th scope="col" className="px-3 py-5 font-medium">
+                                                    Action
+                                                </th>
 
-                                            <th scope="col" className="px-3 py-5 font-medium">
-                                                Login
-                                            </th>
-                                        </tr>
-                                    </thead>
-                                    <tbody className="bg-white">
-                                        {
-                                            response && response.customers && response.customers.map((user) =>
-                                                <tr key={user._id} className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg " >
-                                                    <td className="w-fit whitespace-nowrap py-3 pl-6 pr-3">
-                                                        {user._id}
-                                                    </td>
-                                                    <td className="whitespace-nowrap px-3 py-3">
-                                                        {user.firstname + ' ' + user.lastname}
-                                                    </td>
-                                                    <td className="whitespace-nowrap px-3 py-3">
-                                                        {user.username}
-                                                    </td>
-                                                    <td className="whitespace-nowrap px-3 py-3">
-                                                        {user.email}
-                                                    </td>
-                                                    <td className="whitespace-nowrap px-3 py-3">
-                                                        {(user.isActive) ? <Icon name={"activestatus"} /> : <Icon name={"inactivestatus"} />}
-                                                    </td>
-                                                    <td className="whitespace-nowrap px-3 py-3">
-                                                        {user.createdAt}
-                                                    </td>
-                                                    <td className="whitespace-nowrap px-3 py-3 flex ">
-                                                        <span className="pl-3">
-                                                            <ActionButtons name={'Edit'} isclicked={false} pointto={user._id} />
-                                                        </span>
-                                                        <span className="pl-3">
-                                                            <DeleteCustomer id={user._id} />
-                                                        </span>
-                                                    </td>
+                                                <th scope="col" className="px-3 py-5 font-medium">
+                                                    Login
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody className="bg-white">
+                                            {
+                                                response && response.customers && response.customers.map((user) =>
+                                                    <tr key={user._id} className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg " >
+                                                        <td className="w-fit whitespace-nowrap py-3 pl-6 pr-3">
+                                                            {user._id}
+                                                        </td>
+                                                        <td className="whitespace-nowrap px-3 py-3">
+                                                            {user.firstname + ' ' + user.lastname}
+                                                        </td>
+                                                        <td className="whitespace-nowrap px-3 py-3">
+                                                            {user.username}
+                                                        </td>
+                                                        <td className="whitespace-nowrap px-3 py-3">
+                                                            {user.email}
+                                                        </td>
+                                                        <td className="whitespace-nowrap px-3 py-3">
+                                                            {(user.isActive) ? <Icon name={"activestatus"} /> : <Icon name={"inactivestatus"} />}
+                                                        </td>
+                                                        <td className="whitespace-nowrap px-3 py-3">
+                                                            {user.createdAt}
+                                                        </td>
+                                                        <td className="whitespace-nowrap px-3 py-3 flex ">
+                                                            <span className="pl-3">
+                                                                <ActionButtons name={'Edit'} isclicked={false} pointto={user._id} />
+                                                            </span>
+                                                            <span className="pl-3">
+                                                                <DeleteCustomer id={user._id} />
+                                                            </span>
+                                                        </td>
 
-                                                    <td className="whitespace-nowrap px-3 py-3">
-                                                        <Icon name="loginbyadmin" />
-                                                    </td>
-                                                </tr>
-                                            )
-                                        }
-                                    </tbody>
-                                </table>
+                                                        <td className="whitespace-nowrap px-3 py-3">
+                                                            <Icon name="loginbyadmin" />
+                                                        </td>
+                                                    </tr>
+                                                )
+                                            }
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </>
             )}
         </>
     )
--- src/app/ui/common/svg-tiles.tsx
+++ src/app/ui/common/svg-tiles.tsx
@@ -1,5 +1,5 @@
 
-const iconNames = ['edit', 'delete', 'loginbyadmin', 'userstatus', 'Home', 'Wishlist', 'SignOut', 'Briefcase', 'Purchase', 'Arrowright'];
+const iconNames = ['edit', 'delete', 'loginbyadmin', 'userstatus', 'Home', 'Wishlist', 'SignOut', 'Briefcase', 'Purchase', 'Arrowright', 'Search'];
 type IconName = typeof iconNames[number];
 
 export default function Icon({ name }: { name: IconName }) {
@@ -60,6 +60,12 @@ export default function Icon({ name }: { name: IconName }) {
         Arrowright: (
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+            </svg>
+
+        ),
+        Search: (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
             </svg>
 
         )
